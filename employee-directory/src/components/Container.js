78c984757f5fbb9e3183b4e8894f5222cd86f07b
GusGo <gus.gonzalez.15@gmail.com>
2020-04-26 23:19:44 -0400

    comtainer component init

---
+++ employee-directory/src/components/Container.js
@@ -0,0 +1,13 @@
+import React, { component } from "react";
+import Header from "./Header";
+import getEmployees from "../utils/getEmployees";
+
+class Container extends component {
+    state = {
+
+    };
+
+
+};
+
+export default Container;
