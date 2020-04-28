6c13d99e1e50b855ca084d31a12708c2da26a57b
GusGo <gus.gonzalez.15@gmail.com>
2020-04-27 21:58:18 -0400

    Updates

---
+++ precache-manifest.c3c528cf69bdf877392a82d4c072cefb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f4c0b2de0ffc070c657b73e0adc50d",
+    "url": "/HW19-employee-directory/index.html"
+  },
+  {
+    "revision": "7d8b989064553ed1cc77",
+    "url": "/HW19-employee-directory/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c21ecd34ab505e3e75fc",
+    "url": "/HW19-employee-directory/static/js/2.dba9049e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW19-employee-directory/static/js/2.dba9049e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8b989064553ed1cc77",
+    "url": "/HW19-employee-directory/static/js/main.4a910546.chunk.js"
+  },
+  {
+    "revision": "9252650dedd86e6a8b22",
+    "url": "/HW19-employee-directory/static/js/runtime-main.3a77f50f.js"
+  }
+]);
