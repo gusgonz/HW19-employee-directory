a11df2dea85dac2e733a5e3ae96d5ddc236c2148
GusGo <gus.gonzalez.15@gmail.com>
2020-04-27 21:40:49 -0400

    row component using bootstrap

--- employee-directory/src/components/Row.js
+++ employee-directory/src/components/Row.js
@@ -1,2 +1,25 @@
 import React from "react";
 
+function Row(props) {
+    return (
+        <div className="row">
+            <div className="col">
+                {props.picture ? <img alt="Employee" src={props.picture} /> : <h2>Picture</h2>}
+            </div>
+            <div className="col">
+                {props.name ? <p>{props.name}</p> : <h2>Name</h2>}
+            </div>
+            <div className="col">
+                {props.email ? <p>{props.email}</p> : <h2>Email</h2>}
+            </div>
+            <div className="col">
+                {props.number ? <p>{props.number}</p> : <h2>Number</h2>}
+            </div>
+            <div className="col">
+                {props.country ? <p>{props.country}</p> : <h2>Country</h2>}
+            </div>
+        </div>
+    );
+};
+
+export default Row;
