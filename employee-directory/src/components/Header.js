87ca3a793ab2056c94b69630dcd24288dce04220
GusGo <gus.gonzalez.15@gmail.com>
2020-04-27 21:40:59 -0400

    header component

--- employee-directory/src/components/Header.js
+++ employee-directory/src/components/Header.js
@@ -1,30 +1,42 @@
 import React from "react";
 
-function Header() {
+function Header(props) {
     return (<div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">Employee Directory</a>
+            <h1 className="navbar-brand" href="#">Employee Directory</h1>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <li className="nav-item dropdown" key="keylee" >
+                        <p className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Sort By
-                            </a>
+                            </p>
                         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="#">Name</a>
-                            <a className="dropdown-item" href="#">Email</a>
-                            <a className="dropdown-item" href="#">Phone Number</a>
+                            <button className={(props.currentSort === "name") ? "dropdown-item active" : "dropdown-item"} name="name" onClick={props.handleSort}>Name</button>
+                            <button className={(props.currentSort === "email") ? "dropdown-item active" : "dropdown-item"} name="email" onClick={props.handleSort}>Email</button>
+                            <button className={(props.currentSort === "country") ? "dropdown-item active" : "dropdown-item"} name="country" onClick={props.handleSort}>Country</button>
                         </div>
                     </li>
+                    {
+                        (props.currentSort === "") ?
+                            ""
+                            :
+                            (<li className="nav-item">
+                                <button className="nav-link primary" onClick={props.clearSort}>Clear Sort</button>
+                            </li>)
+                    }
+
                 </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
+
+                <input
+                    className="form-control mr-sm-2"
+                    type="search"
+                    onChange={props.handleSearch}
+                    placeholder="Search"
+                    aria-label="Search" />
             </div>
         </nav>
     </div>
